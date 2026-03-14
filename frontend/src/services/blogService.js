1a95d128d774424d00b03bd251ec768f6bdb21f1
SasikalaThennarasu5 <sasikalathennarasu5@gmail.com>
2026-03-14 22:20:29 +0530

    add

--- frontend/src/services/blogService.js
+++ frontend/src/services/blogService.js
@@ -1,6 +1,6 @@
 import api from "./api";
 
 export const getBlogs = async () => {
-  const res = await fetch("http://localhost:8000/api/blogs/");
+  const res = await fetch("https://internship-3-grocerystore-2.onrender.com/api/blogs/");
   return res.json();
 };
